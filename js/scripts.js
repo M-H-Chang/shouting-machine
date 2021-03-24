927de86b44ed40445668e309996d3716c2dc13b7
Vanessa Su <vnessa.su@gmail.com>
2021-03-24 13:15:28 -0400

    Collect user input and display it on the page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-  const personInput = $("input#person").val();
   $("#formOne").submit(function () {
-    $(".person").append("personInput")
-  })
-})
+    const personInput = $("input#person").val();
+    $(".person").append(personInput);
+    event.preventDefault();
+  });
+});
